80ca479cdcc9d819232d84e8d639a399c79899cf
kushal-sinha <kushal147258369@gmail.com>
2025-08-27 18:00:31 +0530

    Created ContactDatabase with Singleton pattern and RoomDB integration.

---
+++ app/src/main/java/com/example/contact_manager_app/ContactDatabase.java
@@ -0,0 +1,24 @@
+package com.example.contact_manager_app;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Contacts.class}, version = 1)
+
+public abstract class ContactDatabase extends RoomDatabase {
+    public abstract ContactDAO getContactDAO();
+
+    // Singleton Design Pattern.
+    private static ContactDatabase dbInstance;
+
+    public static synchronized ContactDatabase getInstance(Context context) {
+        if (dbInstance == null) {
+            dbInstance = Room.databaseBuilder(context.getApplicationContext(), ContactDatabase.class, "contacts_db").fallbackToDestructiveMigration().build();
+        }
+        return dbInstance;
+    }
+
+}
